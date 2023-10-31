716458cb964e518364dce6d39737c4c46e40959f
vsyncv <95345080+vsyncv@users.noreply.github.com>
2023-10-31 08:19:40 -0700

    Update Text2Speech.py

--- Text2Speech.py
+++ Text2Speech.py
@@ -3,51 +3,56 @@ import streamlit as st
 from gtts import gTTS
 from pydub import AudioSegment
 
-def synthesize_text(text, language):
-    try:
-        if text:
-            tts = gTTS(text=text, lang=language)
-            return tts.save("temp.mp3")
-    except Exception as e:
-        st.error("Error occurred during text synthesis.")
+class TextToSpeechApp:
+    def __init__(self):
+        self.default_text = ""
+        self.default_language = 'en'  # English
+        self.temp_file = "temp.mp3"
 
-def process_audio_file(filename):
-    try:
-        audio = AudioSegment.from_file(filename)
+    def synthesize_text(self, text: str, language: str) -> None:
+        try:
+            if text:
+                tts = gTTS(text=text, lang=language)
+                tts.save(self.temp_file)
+        except Exception as e:
+            st.error(f"Error occurred during text synthesis: {e}")
 
-                # Modify the audio speed
+    def process_audio_file(self, filename: str, speed: float) -> str:
+        try:
+            audio = AudioSegment.from_file(filename)
+            audio = self.adjust_audio_speed(audio, speed)
+            
+            # Further audio processing can be added here
+
+            # Exporting the processed audio
+            processed_filename = os.path.splitext(filename)[0] + '_processed.wav'
+            audio.export(processed_filename, format='wav', parameters=['-f', 'wav', '-ac', '1', '-ar', '11025', '-acodec', 'pcm_u8'])
+            return processed_filename
+        except Exception as e:
+            st.error(f"Error occurred during audio processing: {e}")
+            return None
+
+    @staticmethod
+    def adjust_audio_speed(audio: AudioSegment, speed: float) -> AudioSegment:
         if speed > 1:
-            audio = audio.speedup(playback_speed=speed)
+            return audio.speedup(playback_speed=speed)
         elif speed < 1:
-            audio = audio.slowdown(playback_speed=speed)
+            return audio.slowdown(playback_speed=speed)
+        return audio
 
-        # Modify the audio as needed (e.g., convert to mono, adjust volume, remove silent sections)
-        # Add your audio processing logic here
+    def run(self):
+        # UI components for Streamlit
+        announcement_text = st.text_input("Enter Announcement Text", self.default_text)
+        language = st.selectbox("Select Language", ['en', 'fr', 'es'], index=0)
+        speed = st.slider("Select Speed", min_value=0.5, max_value=2.0, step=0.1, value=1.0)
 
-        # Export the processed audio as WAV with 8-bit encoding
-        processed_filename = os.path.splitext(filename)[0] + '_processed.wav'
-        audio.export(processed_filename, format='wav', parameters=['-f', 'wav', '-ac', '1', '-ar', '11025', '-acodec', 'pcm_u8'])
-        print(f'Processed audio file saved: {processed_filename}')
-        return processed_filename
-    except Exception as e:
-        st.error("Error occurred during audio processing.")
-
-# Set the default announcement text and language
-default_text = ""
-default_language = 'en'  # English
-
-# Create input fields for announcement text, language, and speed
-announcement_text = st.text_input("Enter Announcement Text", default_text)
-language = st.selectbox("Select Language", ['en', 'fr', 'es'], index=0)
-speed = st.slider("Select Speed", min_value=0.5, max_value=2.0, step=0.1, value=1.0)
+        if announcement_text:
+            self.synthesize_text(announcement_text, language)
+            processed_filename = self.process_audio_file(self.temp_file, speed)
+            if processed_filename:
+                st.audio(processed_filename, format='audio/wav')
 
 
-# Synthesize text to speech
-synthesize_text(announcement_text, language)
-
-# Process the audio file
-processed_filename = process_audio_file("temp.mp3")
-
-if processed_filename:
-    # Display an audio player for the processed audio file
-    st.audio(processed_filename, format='audio/wav')
+if __name__ == "__main__":
+    app = TextToSpeechApp()
+    app.run()
